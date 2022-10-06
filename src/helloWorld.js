cff09fd9c2f37f41481b7c38fb411e8806f39a82
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-06 23:33:44 +0200

    components-02

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,9 +1,7 @@
 import React from "react"
 
-class Hello extends React.Component {
+export class Hello extends React.Component {
     render() {
         return <h1>Hello, World!</h1>
     }
 }
-
-export default Hello
