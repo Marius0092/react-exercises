1a93717765e2879c7a62296c3e44fcf3afbce7a7
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-07 00:28:55 +0200

    components-07

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react"
+import { Message } from "./message"
 
 export class Hello extends React.Component {
     render() {
         return (
             <Fragment>
                 <h1>Hello, World!</h1>
-                <p>What a beatiful day!</p>
+                <p>What a beatiful day!</p> 
+                <Message />               
             </Fragment>
         )        
     }
