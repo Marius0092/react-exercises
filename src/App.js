708c708c90d2378fe702b9ce71f41db4dc08803e
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-08 00:06:00 +0200

    props-01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Hello } from "./helloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <Hello />
+                <Welcome name = "Johnny" />
             </div>
         )
     }
