a37e13a55853f28a7194bcd3c7baa961d0a326a4
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-08 00:44:13 +0200

    props-04

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name ={<strong>Johnny</strong>}/>
+                <Welcome name ={<strong>Johnny</strong>} age="30"/>
             </div>
         )
     }
