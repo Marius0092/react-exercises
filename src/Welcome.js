5c6a798ed3b88ea64c51ad4d813f1eedd0443ef2
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-08 00:22:46 +0200

    props-03

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,7 +2,13 @@ import React from "react";
 
 export class Welcome extends React.Component {
     render() {
-        return <h2>Welcome, {this.props.name}</h2>
+        return (
+            <div>
+                <h2>Welcome, {this.props.name}</h2>
+                <p>Your age is {this.props.age}</p>
+            </div>
+
+        ) 
     }
 }
 
