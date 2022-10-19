15f8eb0c0af3fe5fcf00c62ab78bf7ee30cb91dc
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-19 21:27:22 +0200

    lists-06

--- src/TodoList.js
+++ src/TodoList.js
@@ -26,11 +26,18 @@ export class TodoList extends React.Component {
         })
     }
 
+    handleRemove = (key) => {
+        const filteredItems = this.state.items.filter(item => item!==key);
+        this.setState({
+            items:filteredItems
+        })
+    }
+
     render() {
         return (
             <div>
                 <ul>
-                    {this.state.items.map((item) => <li>{item}</li>)}
+                    {this.state.items.map((items, index) => <li key={index}>{items} <button type="button" onClick={() => {this.handleRemove(items)}}>remove</button></li>)}
                 </ul>
                 <div>
                     <input name='todo' type='text' onChange={this.handleInput} value={this.state.newItem}></input>
