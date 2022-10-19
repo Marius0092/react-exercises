47e65c3614f07de108f5e19fca6e68a992aae10d
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-19 22:43:38 +0200

    styling-components-03

--- src/Login.js
+++ src/Login.js
@@ -35,13 +35,16 @@ export class Login extends React.Component {
     }
 
     render() {
+        const btnBackground = {
+            'background-color': this.state.password.length < 8 ? "red" : "green"
+        }
         return (
             <div>
                 <input name='username' value={this.state.username} onChange={this.handleInputChange} />
                 <input name='password' value={this.state.password} type='password' onChange={this.handleInputChange} />
                 <input name='remember' type="checkbox" checked={this.state.remember} onChange={this.handleInputChange} />
                 {/* <button type='submit' disabled={!this.state.username || !this.state.password} onClick={this.handleLogin}>Login</button> */}
-                <button name="loginButton" type="submit" onClick={() => this.onLogin()} disabled={this.state.username === "" && this.state.password === ""}>Login</button>
+                <button name="loginButton" type="submit" style={btnBackground} onClick={() => this.onLogin()} disabled={this.state.username === "" && this.state.password === ""}>Login</button>
                 <button onClick={this.handleReset}>Reset</button>
             </div>
         )
