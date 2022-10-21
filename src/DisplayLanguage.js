ae7cef142a53847b7b8b24fd1b13fcceac4e6b3a
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-21 18:55:59 +0200

    context-02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -3,5 +3,12 @@ import { LanguageContext } from "./LanguageContext";
 
 export function DisplayLanguage() {
     const language = useContext(LanguageContext)
-    return <h3>Current Language: {language}</h3>
+    // return <h3>Current Language: {language}</h3>
+    return(
+        <LanguageContext.Consumer>
+            {language =>{
+                return <h1>Current language: {language}</h1>
+            }}
+        </LanguageContext.Consumer>
+    )
 }
