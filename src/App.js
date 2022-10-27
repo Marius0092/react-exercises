2b3165d7f4ee5cda187ee2da7ae7063fb59f6487
Marius0092 <106385493+Marius0092@users.noreply.github.com>
2022-10-27 23:01:51 +0200

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import React from "react";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { LanguageContext } from "./LanguageContext";
-import { useState } from "react";
-// import { Welcome } from "./Welcome";
-// import { Sum } from "./Sum";
-// import { ClickCounter } from "./ClickCounter";
-// import { Login } from "./Login";
-// import { Counter } from "./Counter";
-// import { GithubUser } from "./GithubUser";
-// import { GithubUserList } from "./GithubUserList";
-// import { HookCounter } from "./UseCounter";
-// import { HookForm } from "./HookForm";
-// import { FetchGithub } from "./useGithubUser";
-// import { CarDetails } from "./CarDetails";
-import { FilteredList } from "./FilteredList";
+import { Route, Routes } from "react-router-dom";
+import { Welcome } from "./Welcome";
+
 
 export function App() {
 
-    const [language, setLanguage] = useState("en");
-
-    function handleChangeLanguage(event) {
-        setLanguage(event.target.value);
-    }
-
     return (
-        <div>
-            <select value={language} onChange={handleChangeLanguage}>
-                <option value="en">English</option>
-                <option value="it">Italiano</option>
-            </select>
-            <LanguageContext.Provider value={language}>
-                <DisplayLanguage />
-            </LanguageContext.Provider>
-            {/* <Welcome />
-            <Sum />
-            <ClickCounter onCounterChange={(counter) => {
-                return console.log(`current value is ${counter}`)
-            }} />
-            <Login />
-            <Counter />
-            <GithubUser username="Marius0092" />
-            <GithubUserList />
-            <HookCounter />
-            <HookForm />
-            <FetchGithub username="Marius0092" />
-            <CarDetails initialData={{model: "Mercedes", year: "2022", color: "Gray"}} /> */}
-            <FilteredList />
-        </div>
+        <Routes>
+            <Route path="/" element={<Welcome name = "Mario" />} />       
+        </Routes>
     )
 }
