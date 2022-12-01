1b2bda01e990208f6fe3c7163d3664a08fdfea81
Mario Montante <mariom0092@gmail.com>
2022-12-01 02:35:17 +0100

    react-router-04

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
@@ -8,10 +8,18 @@ import { Welcome } from "./Welcome";
 export function App() {
 
     return (
-        <Routes>
-            <Route path="/" element={<Welcome name="Mario" />} />
-            <Route path="/counter" element={<Counter />} />
-            <Route path="users/:username" element={<ShowGithubUser/>}/>
-        </Routes>
+        <div>
+            <Routes>
+                <Route path="/" element={<Welcome name="Mario" />} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="users/:username" element={<ShowGithubUser />} />
+            </Routes>
+
+            <div>
+                <Link to="./"><h2>Home</h2></Link>
+                <Link to="./Counter"><h2>Counter</h2></Link>
+                <Link to="users/:username"><h2>User</h2></Link>
+            </div>
+        </div>
     )
 }
